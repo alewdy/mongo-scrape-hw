67ec17ad9f72ce503d01fc17a1ce43ceafc30a43
Alewdy <alewdys@gmail.com>
2018-09-20 13:31:22 -0400

    update

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var bodyParser = require("body-parser");
+var logger = require("logger");
+var mongoose = require("mongoose");
+var path = require("path");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+//Requires all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 3000
+var app = express();
+ 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
 // Listen on port
-app.listen(port, function() {
-    console.log("App running on port " + port);
+app.listen(PORT, function() {
+    console.log("App running on port https://localhost:" + PORT);
   });
