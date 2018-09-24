a97d37f693e0a773a666aa15beea5a3979e1b48b
butterrzz <38452206+butterrzz@users.noreply.github.com>
2018-09-23 20:07:34 -0400

    hw updates

--- models/Note.js
+++ models/Note.js
@@ -1,12 +1,21 @@
+// Require mongoose
 var mongoose = require("mongoose");
-
+// Create a schema class
 var Schema = mongoose.Schema;
 
+// Create the Note schema
 var NoteSchema = new Schema({
-  text: String
+    body: {
+        type: String
+    },
+    article: {
+        type: Schema.Types.ObjectId,
+        ref: "Article"
+    }
 });
 
+// Create the Note model with the NoteSchema
 var Note = mongoose.model("Note", NoteSchema);
 
-
-module.exports = Note; // Export the Note model
+// Export the Note model
+module.exports = Note;
